0d207c06ffe370dc679cb0575973ba7ed7bd8320
keshar <paudelkeshar2055@gmail.com>
2019-10-11 07:58:15 +0545

    basics of git initialized.

---
+++ src/main/java/com/keshar/restwebservices/basicsofREST/BasicsofRestApplication.java
@@ -0,0 +1,13 @@
+package com.keshar.restwebservices.basicsofREST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicsofRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicsofRestApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/keshar/restwebservices/basicsofREST/HelloWorldBean.java
@@ -0,0 +1,26 @@
+package com.keshar.restwebservices.basicsofREST;
+
+public class HelloWorldBean {
+	
+	private String message;
+
+	public HelloWorldBean(String message) {
+		this.message=message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return "HelloWorldBean [message=" + message + "]";
+	}
+	
+	
+
+}
